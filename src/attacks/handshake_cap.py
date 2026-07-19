9597d426e0012c4ed7c4503cfc97bc68084802c4
ImNotASkidLolus <igor_niedbala@op.pl>
2026-07-19 20:57:39 +0200

    fix handshake parsing as hcxpcapng has problems with filtering :(

--- src/attacks/handshake_cap.py
+++ src/attacks/handshake_cap.py
@@ -14,14 +14,19 @@ class capture:
         self.eapol_counter = 0
     def stop(self):
         self._stop.set()
-    def check_packet(self, packet) -> bool:
-        self.found_packets.append(packet)
-        if packet.haslayer("EAPOL"):
-            self.eapol_counter += 1
+    def check_packet(self, packet):
+        if packet.haslayer("Dot11"):
+            ad1 = packet.addr1
+            ad2 = packet.addr2
+            ad3 = packet.addr3
+            if ad1 == globals.selected_bssid.lower() or ad2 == globals.selected_bssid.lower() or ad3 == globals.selected_bssid.lower():
+                self.found_packets.append(packet)
+            if packet.haslayer("EAPOL"):   
+                self.eapol_counter += 1
 
     def capture_handshakes(self):
-        scapy.sniff(filter=f"ether host {globals.selected_bssid}", iface = globals.interface, stop_filter= lambda p: self.eapol_counter >= 4 or self._stop.is_set())
-        if any(p.haslayer("EAPOL") for p in self.found_packets):
+        scapy.sniff(prn=self.check_packet, iface = globals.interface, stop_filter= lambda p: self.eapol_counter >= 4 or self._stop.is_set())
+        if self.eapol_counter >= 4:
             scapy.wrpcap("capture.pcap", self.found_packets)
             self.found_handshake = True
 
