59c9bfffd33a992ae0657b499353be735f3b55cb
ImNotASkidLolus <igor_niedbala@op.pl>
2026-07-19 20:04:35 +0200

    fix: instead of saving 1 EAPOL packet it saves the whole handshake now

--- src/attacks/handshake_cap.py
+++ src/attacks/handshake_cap.py
@@ -11,15 +11,16 @@ class capture:
         self.file = "capture.pcap"
         self.found_packets = []
         self.found_handshake = False
+        self.eapol_counter = 0
     def stop(self):
         self._stop.set()
     def check_packet(self, packet) -> bool:
         self.found_packets.append(packet)
         if packet.haslayer("EAPOL"):
-            return True
-        return False
-    def capture_handshakes(self) -> bool:
-        scapy.sniff(filter=f"ether host {globals.selected_bssid}", iface = globals.interface, stop_filter= lambda p: self.check_packet(p) or self._stop.is_set(), timeout = 600)
+            self.eapol_counter += 1
+
+    def capture_handshakes(self):
+        scapy.sniff(filter=f"ether host {globals.selected_bssid}", iface = globals.interface, stop_filter= lambda p: self.eapol_counter >= 4 or self._stop.is_set())
         if any(p.haslayer("EAPOL") for p in self.found_packets):
             scapy.wrpcap("capture.pcap", self.found_packets)
             self.found_handshake = True
