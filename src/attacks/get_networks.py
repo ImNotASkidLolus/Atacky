ff51b767b641ea19be256d70f4d767da497391b1
ImNotASkidLolus <igor_niedbala@op.pl>
2026-08-25 19:00:34 +0100

    fix bugs

--- src/attacks/get_networks.py
+++ src/attacks/get_networks.py
@@ -35,8 +35,7 @@ def packet_handler(packet):
                     return
             privacy = packet[scapy.Dot11Elt:4].info
             privacy = int(privacy[1])
-            RSN_INFO = packet[scapy.Dot11Elt:48].info
-            if RSN_INFO:
+            if packet[scapy.Dot11Elt:48].info:
                 privacy = 3
             ssid.replace("\0", "")
             ssid.replace("\x00", "")
